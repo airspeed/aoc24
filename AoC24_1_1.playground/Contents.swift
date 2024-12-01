d636ce977753005b4e5b699e140f791a02cd20ea
Francesco Puglisi <francesco.puglisi@gmx.de>
2024-12-01 22:45:53 +0100

    Day 1

---
+++ AoC24_1_1.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+import Foundation
+
+let input = """
+<redacted>
+"""
+let lines =
+input.split(separator: "\n")
+    .map({ $0.split(separator: " ")
+    .compactMap({ Int($0) }) })
+let firstList =
+lines.compactMap(\.first)
+.sorted()
+let secondList =
+lines.compactMap(\.last)
+.sorted()
+let differences =
+zip(firstList, secondList)
+    .map({ abs($0.0 - $0.1) })
+let result1 =
+differences
+    .reduce(0, +)
+let similarity =
+firstList.map { score in
+    score * secondList.filter { $0 == score }.count
+}
+let result2 =
+similarity.reduce(0, +)
+print(result1)
+print(result2)
